f4c68745554bf9626dc1205b506e6b70848fde81
dillon-todd-dev <dillontodd.dev@gmail.com>
2024-10-12 18:56:09 -0500

    remove spring boot dependencies

--- src/main/java/com/dillontodd/jsonparser/JsonparserApplication.java
+++ src/main/java/com/dillontodd/jsonparser/JsonparserApplication.java
@@ -1,13 +1,9 @@
 package com.dillontodd.jsonparser;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class JsonparserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JsonparserApplication.class, args);
+		System.out.println("hello world");
 	}
 
 }
