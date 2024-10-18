19c512ab517b92fe267f6305dea31cf1279b4d60
dillon-todd-dev <dillontodd.dev@gmail.com>
2024-10-18 08:27:50 -0500

    add tests for step 1

--- src/test/java/com/dillontodd/jsonparser/JsonIOTest.java
+++ src/test/java/com/dillontodd/jsonparser/JsonIOTest.java
@@ -1,13 +1,22 @@
 package com.dillontodd.jsonparser;
 
+import java.io.InputStream;
+
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class JsonIOTest {
 
     @Test
-    public void basicTest() {
-        Assertions.assertTrue(JsonIO.parseJson("{}"));
-        Assertions.assertFalse(JsonIO.parseJson("{"));
+    public void step1Test() throws Exception {
+        InputStream validStream = getClass().getResourceAsStream("/tests/step1/valid.json");
+        InputStream invalidStream = getClass().getResourceAsStream("/tests/step1/invalid.json");
+        String validJson = new String(validStream.readAllBytes());
+        String invalidJson = new String(invalidStream.readAllBytes());
+        validStream.close();
+        invalidStream.close();
+
+        Assertions.assertTrue(JsonIO.parseJson(validJson));
+        Assertions.assertFalse(JsonIO.parseJson(invalidJson));
     }
 }
